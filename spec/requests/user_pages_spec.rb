b16b5b78c79684186329ab19ad97c851a6a13dbe
hiyamamo <h1r0ki_yamam0t0@yahoo.co.jp>
2014-08-14 22:13:19 +0900

    Finish 8 show

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,12 +33,7 @@ describe "UserPages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",               with: "Example User"
-        fill_in "Email",              with: "user@example.com"
-        fill_in "Password",           with:"foobar"
-        fill_in "Confirmation",       with: "foobar"
-      end
+      before { valid_singup }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -50,7 +45,7 @@ describe "UserPages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_succsess_message("Welcome") }
         let(:user) { User.find_by(email: "user@example.com") }
 
       end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,4 +7,30 @@ include ApplicationHelper
 #    "#{base_title} | #{page_title}"
 #  end
 #end
+#
 
+def valid_signin(user)
+  fill_in "Email",  with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+Rspec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text:message)
+  end
+end
+
+
+Rspec::Matchers.define :have_succsess_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text:message)
+  end
+end
+
+def valid_singup
+  fill_in "Name",               with: "Example User"
+  fill_in "Email",              with: "user@example.com"
+  fill_in "Password",           with: "foobar"
+  fill_in "Confirmation",       with: "foobar"
+end
